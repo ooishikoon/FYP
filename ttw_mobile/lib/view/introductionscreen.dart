4f097440329ef202692095144c2dd6cb42cd026b
ooishikoon <skooi0307@hotmail.com>
2023-03-31 14:57:49 +0800

    Update introduction page
    Speak out the introduction page, click any place of the page will stop the speech

--- ttw_mobile/lib/view/introductionscreen.dart
+++ ttw_mobile/lib/view/introductionscreen.dart
@@ -56,106 +56,110 @@ class _IntroductionScreenState extends State<IntroductionScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    screenHeight = MediaQuery.of(context).size.height;
-    screenWidth = MediaQuery.of(context).size.width;
-    if (screenWidth <= 600) {
-      resWidth = screenWidth;
-    } else {
-      resWidth = screenWidth * 0.75;
-    }
-
-    return Scaffold(
-      body: Stack(alignment: Alignment.center, children: [
-        Container(
-            decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/introduction_page.png'),
-              fit: BoxFit.fill),
-        )),
-        Column(
+  Widget build(BuildContext context) => Scaffold(
+        body: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 20, 20, 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: const <Widget>[
-                  IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.headphones,
-                        size: 35,
-                        color: Colors.black,
-                      )),
-                ],
+            buildBackground(),
+            buildContext(),
+            GestureDetector(
+              onTap: () {
+                _stop();
+              },
+            ),
+            buildSkipButton(),
+          ],
+        ),
+      );
+
+  Widget buildBackground() => ClipRRect(
+        child: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/introduction_page.png'),
+                    fit: BoxFit.cover))),
+      );
+
+  Widget buildContext() => Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 20, 20, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const <Widget>[
+                IconButton(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.headphones,
+                      size: 35,
+                      color: Colors.black,
+                    )),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(200),
+              child: const Image(
+                image: AssetImage('assets/images/logo.png'),
+                fit: BoxFit.fill,
               ),
             ),
-            SizedBox(
-              height: 150,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(200),
-                child: const Image(
-                  image: AssetImage('assets/images/logo.png'),
-                  fit: BoxFit.fill,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Padding(
+                padding: EdgeInsets.fromLTRB(40, 20, 40, 30),
+                child: Text(
+                  "\t\t\t\t\tTell the World is a text to speech mobile application to provide the user a listening tool that aims to assist the visually impaired to read the content by converting the text into speech."
+                  "\n\n\t\t\t\t\tTell the World helps them to read the content via listening by importing documents and dictionaries to check meaning while reading.",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(40, 20, 40, 30),
-                  child: Text(
-                    "\t\t\t\t\tTell the World is a text to speech mobile application to provide the user a listening tool that aims to assist the visually impaired to read the content by converting the text into speech."
-                    "\n\n\t\t\t\t\tTell the World helps them to read the content via listening by importing documents and dictionaries to check meaning while reading.",
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
+            ],
+          ),
+        ],
+      );
+
+  Widget buildSkipButton() => Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 20, 20, 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.skip_next,
+                    size: 20.0,
                   ),
+                  label: const Text('Skip'),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    minimumSize: const Size(100, 40),
+                  ),
+                  onPressed: () {
+                    _stop();
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (content) => MainScreen(
+                                  user: user,
+                                )));
+                  },
                 ),
               ],
             ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 20, 20, 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton.icon(
-                    icon: const Icon(
-                      Icons.skip_next,
-                      size: 20.0,
-                    ),
-                    label: const Text('Skip'),
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50.0),
-                      ),
-                      minimumSize: const Size(100, 40),
-                    ),
-                    onPressed: () {
-                      _stop();
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (content) => MainScreen(
-                                    user: user,
-                                  )));
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ]),
-    );
-  }
+          ),
+        ],
+      );
 
   // //Method to convert text to speech
   // _speakText() async {
