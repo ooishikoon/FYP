011f916739c185d2be53161c1e9e6f6e8dbcb4ff
ooishikoon <skooi0307@hotmail.com>
2023-05-04 15:54:55 +0800

    add the image and pdf into server folder

--- ttw_mobile/server/php/saveImage.php
+++ ttw_mobile/server/php/saveImage.php
@@ -10,16 +10,37 @@ include_once("dbconnect.php");
 // Get the user email and image data from the POST request
 $user_email = $_POST['email'];
 $file_name = $_POST['filename'];
-$image_data = base64_decode($_POST['image']);
+$image_data = $_POST['image'];
 
 // Prepare the SQL statement
 $stmt = mysqli_prepare($conn, 'INSERT INTO tbl_file (file_id, file_name, file, user_email) VALUES (?, ?, ?, ?)');
 
 // Set the parameters and bind them to the statement
+$file_id = null; // file_id is auto-incremented
 mysqli_stmt_bind_param($stmt, 'ssss', $file_id, $file_name, $image_data, $user_email);
 
-// Execute the statement
-mysqli_stmt_execute($stmt);
+// Execute the statement and check for errors
+if (!mysqli_stmt_execute($stmt)) {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+    die();
+}
+
+// Get the ID of the inserted row
+$file_id = mysqli_insert_id($conn);
+
+// Save the file to a folder
+$uploads_dir = __DIR__ . '/../assets/image/';
+if (!file_exists($uploads_dir)) {
+    mkdir($uploads_dir, 0777, true);
+}
+$file_path = $uploads_dir . $file_id . '.jpg';
+$image_data = base64_decode($image_data);
+if (!file_put_contents($file_path, $image_data)) {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+    die();
+}
 
 // Close the statement and the database connection
 mysqli_stmt_close($stmt);
@@ -27,5 +48,10 @@ mysqli_close($conn);
 
 // Return a JSON response
 $response = array('status' => 'success');
-echo json_encode($response);
+sendJsonResponse($response);
+
+function sendJsonResponse($response) {
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
 ?>
--- ttw_mobile/server/php/savePdf.php
+++ ttw_mobile/server/php/savePdf.php
@@ -7,7 +7,7 @@ if (!isset($_POST)) {
 
 include_once("dbconnect.php");
 
-// Get POST data
+// Get the user email and image data from the POST request
 $user_email = $_POST['email'];
 $file_name = $_POST['filename'];
 $file = $_POST['file_data'];
@@ -19,15 +19,41 @@ $pdf_data = base64_decode($file);
 $stmt = mysqli_prepare($conn, 'INSERT INTO tbl_file (file_id, file_name, file, user_email) VALUES (?, ?, ?, ?)');
 
 // Set the parameters and bind them to the statement
+$file_id = null; // file_id is auto-incremented
 mysqli_stmt_bind_param($stmt, 'ssss', $file_id, $file_name, $pdf_data, $user_email);
 
-// Execute SQL statement
-if ($stmt->execute()) {
-  // Return success message
-  echo "PDF saved successfully.";
-} else {
-  // Return error message
-  echo "Failed to save PDF.";
+// Execute the statement and check for errors
+if (!mysqli_stmt_execute($stmt)) {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+    die();
 }
 
+// Get the ID of the inserted row
+$file_id = mysqli_insert_id($conn);
+
+// Save the file to a folder
+$uploads_dir = __DIR__ . '/../assets/pdf/';
+if (!file_exists($uploads_dir)) {
+    mkdir($uploads_dir, 0777, true);
+}
+$file_path = $uploads_dir . $file_id . '.pdf';
+if (!file_put_contents($file_path, $pdf_data)) {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+    die();
+}
+
+// Close the statement and the database connection
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
+
+// Return a JSON response
+$response = array('status' => 'success');
+sendJsonResponse($response);
+
+function sendJsonResponse($response) {
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
 ?>
